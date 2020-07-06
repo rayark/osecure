c84e4aa0532debce760dacf2a3d94b0e77c4ab19
fieliapm <fieliapm@gmail.com>
2020-07-06 18:07:21 +0800

    use StringSet to store app IDs and permissions

--- data_structure.go
+++ data_structure.go
@@ -1,12 +1,38 @@
 package osecure
 
-type set map[string]struct{}
+type StringSet map[string]struct{}
 
-func (s set) add(x string) {
+func NewStringSet(a []string) StringSet {
+	s := make(StringSet)
+
+	for _, x := range a {
+		s.Add(x)
+	}
+
+	return s
+}
+
+func (s StringSet) Add(x string) {
 	s[x] = struct{}{}
 }
 
-func (s set) contain(x string) bool {
+func (s StringSet) Remove(x string) {
+	delete(s, x)
+}
+
+func (s StringSet) Contain(x string) bool {
 	_, ok := s[x]
 	return ok
+}
+
+func (s StringSet) List() []string {
+	a := make([]string, len(s))
+
+	i := 0
+	for x := range s {
+		a[i] = x
+		i++
+	}
+
+	return a
 }
