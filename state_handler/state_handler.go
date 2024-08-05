7e13604e0cfad7a18a2da1a06450019ca3446c5c
Vicchang <victor410165@hotmail.com>
2024-08-05 10:34:04 +0800

    chore: rename variable for correctness

--- state_handler/state_handler.go
+++ state_handler/state_handler.go
@@ -88,10 +88,10 @@ func (sh DefaultStateHandler) Generate(cookieStore *sessions.CookieStore, w http
 
 	// retrieve redirect url from query in order to let frontend decide where to redirect
 	q := r.URL.Query()
-	redirectUrl := q.Get(continueParam)
+	continueArg := q.Get(continueParam)
 	var err error
-	if redirectUrl != "" {
-		continueURI = redirectUrl
+	if continueArg != "" {
+		continueURI = continueArg
 	}
 
 	// generate nonce
